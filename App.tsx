ff85315c1d3d710086ec59d4306fc94e08a07b58
Roeunbora Tharath <roeunboratharath@gmail.com>
2024-05-30 00:14:02 +0700

    Splashscreen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import AppContainer from './src';
 import './src/Translation';
 import {StyleSheet} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
-const App: React.FC = () => (
-  <GestureHandlerRootView style={styles.container}>
-    <AppContainer />
-  </GestureHandlerRootView>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <AppContainer />
+    </GestureHandlerRootView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
